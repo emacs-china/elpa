c29deebdd7924d0d042502e05eaa3cd9b43b15a3
Chunyang Xu <xuchunyang.me@gmail.com>
2016-05-11 18:47:46 +0800

    Download signature files for GNU ELPA

--- extract-package-url.el
+++ extract-package-url.el
@@ -19,24 +19,34 @@
     (org          . "http://orgmode.org/elpa/"))
   "Mapping of source name and url.")
 
-(let* ((name (getenv "elpa"))
-       (url
-        (and name
-             (cdr (assq (intern name) extract-package-url-source-mapping)))))
-  (unless url (error "Environment variable 'elpa' not set or incorrect"))
-  (let ((ac-url (concat url "archive-contents"))
-        (ac-file (expand-file-name "archive-contents" name)))
-    (url-copy-file ac-url ac-file t)
-    (shell-command (concat "chmod a+r " ac-file))
-    (with-temp-buffer
-      (insert-file-contents ac-file)
-      (dolist (pkg (cdr (read (buffer-string))))
-        (let ((pkg-name (car pkg))
-              (pkg-version (mapconcat #'number-to-string
-                                      (aref (cdr pkg) 0)
-                                      "."))
-              (pkg-type (if (eq 'tar (aref (cdr pkg) 3))
-                            "tar" "el")))
-          (princ (format "%s%s-%s.%s\n" url pkg-name pkg-version pkg-type)))))))
+(defun extract-package-url (elpa)
+  (let* ((name elpa)
+         (url
+          (and name
+               (cdr (assq (intern name) extract-package-url-source-mapping)))))
+    (unless url (error "Unknown ELPA: %s" name))
+    (let ((ac-url (concat url "archive-contents"))
+          (ac-file (expand-file-name "archive-contents" name)))
+      (url-copy-file ac-url ac-file t)
+      (shell-command (concat "chmod a+r " ac-file))
+      ;; Download signature file for GNU ELPA
+      (when (string-equal "gnu" name)
+        (url-copy-file (concat ac-url ".sig") (concat ac-file ".sig") t)
+        (shell-command (concat "chmod a+r " (concat ac-file ".sig"))))
+      (with-temp-buffer
+        (insert-file-contents ac-file)
+        (dolist (pkg (cdr (read (buffer-string))))
+          (let ((pkg-name (car pkg))
+                (pkg-version (mapconcat #'number-to-string
+                                        (aref (cdr pkg) 0)
+                                        "."))
+                (pkg-type (if (eq 'tar (aref (cdr pkg) 3))
+                              "tar" "el")))
+            (princ (format "%s%s-%s.%s\n" url pkg-name pkg-version pkg-type))
+            ;; Download signature file for GNU ELPA
+            (when (string-equal "gnu" name)
+              (princ (format "%s%s-%s.%s.sig\n" url pkg-name pkg-version pkg-type)))))))))
+
+(extract-package-url (getenv "elpa"))
 
 ;;; extract-package-url.el ends here
